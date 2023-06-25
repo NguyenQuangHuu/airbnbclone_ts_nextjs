bbb759bdbaad91b9dc70ca3e78abb5a244b10e14
OS <nguyenquanghuu2010@gmail.com>
2023-06-25 21:28:18 +0700

    first commit

---
+++ airbnb_clone/src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadown-sm">
+        <div className='py-4 border-b-[1px]'>Navbar</div>
+    </div>
+  )
+}
+
+export default Navbar;
